45a6d7532d4e85bb75e21c986b38dcc7a7040e19
dougpuett <dpuett@usertesting.com>
2015-08-30 15:02:34 -0700

    init.el

---
+++ init.el
@@ -0,0 +1,185 @@
+;; path where settings files are kept
+(add-to-list 'load-path "~/.emacs.d/settings")
+;; path to where plugins are kept
+(setq plugin-path "~/.emacs.d/el-get/")
+
+;; path to mac stuff
+(setenv "PATH"
+        (shell-command-to-string "source $HOME/.bash_profile && printf $PATH"))
+
+;; define various custom functions
+(require 'custom-functions)
+
+;; configure general settings
+(require 'general-settings)
+
+;; install dependencies with el-get
+(require 'el-get-settings)
+
+;; thin cursor
+(setq-default cursor-type 'bar) 
+
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+         (next-win-buffer (window-buffer (next-window)))
+         (this-win-edges (window-edges (selected-window)))
+         (next-win-edges (window-edges (next-window)))
+         (this-win-2nd (not (and (<= (car this-win-edges)
+                     (car next-win-edges))
+                     (<= (cadr this-win-edges)
+                     (cadr next-win-edges)))))
+         (splitter
+          (if (= (car this-win-edges)
+             (car (window-edges (next-window))))
+          'split-window-horizontally
+        'split-window-vertically)))
+    (delete-other-windows)
+    (let ((first-win (selected-window)))
+      (funcall splitter)
+      (if this-win-2nd (other-window 1))
+      (set-window-buffer (selected-window) this-win-buffer)
+      (set-window-buffer (next-window) next-win-buffer)
+      (select-window first-win)
+      (if this-win-2nd (other-window 1))))))
+
+;; toggle window split:
+(global-set-key (kbd "C-x |") 'toggle-window-split)
+
+;; prohibits veritical split
+(setq split-height-threshold nil)
+(setq split-width-threshold 0)
+
+;---------------;
+;;; Utilities ;;;
+;---------------;
+
+;; Multiple Cursors
+; (require 'multiple-cursors)
+
+;; Git
+(include-plugin "magit")
+(require 'magit)
+
+;; Popup
+(include-elget-plugin "popup")
+(require 'popup)
+
+;; Websocket
+(include-plugin "websocket")
+(require 'websocket)
+
+;; Request
+(include-plugin "request")
+(require 'request)
+
+;; yasnippet
+(require 'yasnippet-settings)
+
+;; Auto complete
+(require 'auto-complete-settings)
+
+;; Camelcase functions
+(require 'camelcase-settings)
+
+;; Helm
+(require 'helm-settings)
+
+;; melpa
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;-----------;
+;;; Modes ;;;
+;-----------;
+
+;; Autocomplted
+(add-to-list 'load-path "~/.emacs.d/auto-complete-1.3.1")
+(require 'auto-complete)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode t)
+
+;; Ido mode
+(require 'ido)
+(ido-mode 1)
+
+;; MuMaMo
+(require 'mumamo-settings)
+
+;; Markdown mode
+(require 'markdown-settings)
+
+;; Python mode 
+(require 'python-settings)
+; python-mode
+(setq py-install-directory "~/.emacs.d/python-mode.el-6.2.0")
+(add-to-list 'load-path py-install-directory)
+(require 'python-mode)
+
+; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
+
+;; LaTeX and Auctex
+;; (require 'latex-settings)
+
+;; SCSS Mode
+(require 'scss-settings)
+
+;; Matlab mode
+
+
+;; Javascript
+(require 'js-settings)
+
+;; YAML mode
+(require 'yaml-settings)
+
+;; Nyancat mode!
+;; (nyan-mode 1)
+
+
+;---------------------------------------------------------------------
+;; Put auto 'custom' changes in a separate file (this is stuff like
+;; custom-set-faces and custom-set-variables)
+(load 
+ (setq custom-file (expand-file-name "settings/custom.el" user-emacs-directory))
+ 'noerror)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
